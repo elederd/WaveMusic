98494063935bc1b6a0dc328f818db4477960c15e
elederd <mjjg140698@gmail.com>
2024-12-31 21:18:13 -0400

    Update keepAlive.js

--- src/plugin/plugins/keepAlive.js
+++ src/plugin/plugins/keepAlive.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const keepAlive = {
     name: 'KeepAlive Plugin',
@@ -7,9 +9,23 @@ const keepAlive = {
     initialize: (client) => {
         if (client.config.keepAlive) {
             const server = http.createServer((req, res) => {
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end(`I'm alive! Currently serving ${client.guilds.cache.size} guilds.`);
+                if (req.url === '/') {
+                    // Leer el archivo HTML
+                    fs.readFile(path.join(__dirname, 'index.html'), 'utf-8', (err, data) => {
+                        if (err) {
+                            res.writeHead(500, { 'Content-Type': 'text/plain' });
+                            res.end('Error loading the HTML file.');
+                            return;
+                        }
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        res.end(data);
+                    });
+                } else {
+                    res.writeHead(200, { 'Content-Type': 'text/plain' });
+                    res.end(`I'm alive! Currently serving ${client.guilds.cache.size} guilds.`);
+                }
             });
+
             server.listen(3000, () => {
                 client.logger.info('Keep-Alive server is running on port 3000');
             });

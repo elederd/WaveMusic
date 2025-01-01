059f38646ddb90ce1f72dda67be970befda45f88
elederd <mjjg140698@gmail.com>
2025-01-01 16:14:30 -0400

    Update nowplaying.js

--- src/commands/Music/nowplaying.js
+++ src/commands/Music/nowplaying.js
@@ -1,4 +1,9 @@
 const Command = require("../../structures/Command.js");
+const { Riffy } = require("riffy");
+const { EmbedBuilder, AttachmentBuilder } = require("discord.js");
+const { Dynamic } = require("musicard");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Nowplaying extends Command {
   constructor(client) {
@@ -28,26 +33,46 @@ module.exports = class Nowplaying extends Command {
       options: [],
     });
   }
+
   async run(client, ctx) {
     const player = client.queue.get(ctx.guild.id);
     const track = player.current;
-    const position = player.player.position;
-    const duration = track.info.length;
-    const bar = client.utils.progressBar(position, duration, 20);
-    const embed1 = this.client
-      .embed()
-      .setColor(this.client.color.main)
-      .setAuthor({ name: "Now Playing", iconURL: ctx.guild.iconURL({}) })
-      .setThumbnail(track.info.artworkUrl)
-      .setDescription(
-        `[${track.info.title}](${track.info.uri}) - Request By: ${track.info.requester}\n\n\`${bar}\``
-      )
-      .addFields({
-        name: "\u200b",
-        value: `\`${client.utils.formatTime(
-          position
-        )} / ${client.utils.formatTime(duration)}\``,
+
+    // Generate music card with Dynamic
+    try {
+      const musicard = await Dynamic({
+        thumbnailImage: track.info.thumbnail || 'https://example.com/default_thumbnail.png',
+        backgroundColor: '#070707',
+        progress: 10,
+        progressColor: '#FF7A00',
+        progressBarColor: '#5F2D00',
+        name: track.info.title,
+        nameColor: '#FF7A00',
+        author: track.info.author || 'Unknown Artist',
+        authorColor: '#696969',
       });
-    return await ctx.sendMessage({ embeds: [embed1] });
+
+      // Save the generated card to a file
+      const cardPath = path.join(__dirname, 'musicard.png');
+      fs.writeFileSync(cardPath, musicard);
+
+      // Prepare the attachment and embed
+      const attachment = new AttachmentBuilder(cardPath, { name: 'musicard.png' });
+      const embed1 = new EmbedBuilder()
+        .setColor('#FF7A00')
+        .setAuthor({ name: "Now Playing", iconURL: ctx.guild.iconURL({}) })
+        .setDescription(`[${track.info.title}](${track.info.uri}) - Requested By: ${track.info.requester}`)
+        .setImage('attachment://musicard.png')
+        .setFooter({ text: `Duration: ${client.utils.formatTime(track.info.length)}` });
+
+      // Send the embed and attachment
+      return await ctx.sendMessage({ embeds: [embed1], files: [attachment] });
+    } catch (error) {
+      console.error("Error creating music card:", error);
+      const embedError = new EmbedBuilder()
+        .setColor('#FF0000')
+        .setDescription("⚠️ **Unable to generate music card.**");
+      return await ctx.sendMessage({ embeds: [embedError] });
+    }
   }
 };

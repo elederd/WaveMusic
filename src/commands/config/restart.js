3e5891a5f4ca907fe240fb6a9f2f00de4787df37
elederd <mjjg140698@gmail.com>
2025-03-20 22:40:30 -0400

    Update restart.js

--- src/commands/config/restart.js
+++ src/commands/config/restart.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-const RENDER_API_KEY = process.env.APIKEY; // Reemplaza con tu API Key de Render
-const SERVICE_ID = process.env.SERVICEID; // Reemplaza con el ID de tu servicio en Render
+var RENDER_API_KEY = process.env.APIKEY; // Reemplaza con tu API Key de Render
+var SERVICE_ID = process.env.SERVICEID; // Reemplaza con el ID de tu servicio en Render
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,17 +10,16 @@ module.exports = {
     async execute(interaction) {
         await interaction.deferReply();
         try {
-            const response = await fetch(`https://api.render.com/v1/services/${SERVICE_ID}/restart`, {
+            var response = await fetch(`https://api.render.com/v1/services/${SERVICE_ID}/restart`, {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
                     'Authorization': `Bearer ${RENDER_API_KEY}`
                 }
             });
-            if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+            if (!response.ok) throw new Error();
             await interaction.editReply('🔄 El bot se está reiniciando en Render.');
         } catch (error) {
-            console.error('Error al reiniciar:', error.message);
             await interaction.editReply('❌ Error al reiniciar el bot.');
         }
     }
